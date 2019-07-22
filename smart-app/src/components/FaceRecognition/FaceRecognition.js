c07a5c36179b08c0d60b562310412fac57c54ac1
AlexOks <apollo1989@ukr.net>
2019-07-22 17:22:56 +0300

    Add Facerecognition box

--- smart-app/src/components/FaceRecognition/FaceRecognition.js
+++ smart-app/src/components/FaceRecognition/FaceRecognition.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './FaceRecognition.css';
 
-const FaceRecognition = ({imageUrl}) => {
+const FaceRecognition = ({imageUrl, box}) => {
     return(
         <div className='center ma'>
             <div className='absolute mt3'>
-                <img alt='' src={imageUrl} style={{ maxWidth: 500 }} width='100%' height='auto' />
-                <img alt='' src={imageUrl} style={{ maxWidth: 500 }} width='100%' height='auto' />
+                <img id='inputimage' alt='' src={imageUrl} style={{ maxWidth: 500 }} width='100%' height='auto' />
+                <div className ='bounding-box' style={{top:box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
             </div>
         </div>
     );
