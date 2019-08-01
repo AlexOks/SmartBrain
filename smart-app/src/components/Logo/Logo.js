ce6d847514feddf9e12147c8bbc598fef51e82b4
AlexOks <alex.oksyuta@gmail.com>
2019-08-01 13:26:52 +0300

    Testing

--- smart-app/src/components/Logo/Logo.js
+++ smart-app/src/components/Logo/Logo.js
@@ -11,6 +11,11 @@ const Logo = () =>{
                     <img style={{paddingTop: '4px' }}src={face} alt='logo' />
                 </div>
             </Tilt>
+            <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 100, width: 100 }} >
+                <div className="Tilt-inner pa3">
+                    <img style={{paddingTop: '4px' }}src={face} alt='logo' />
+                </div>
+            </Tilt>
         </div>
     );
 }
