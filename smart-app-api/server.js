ffee9e5235ae66c48550245e8849c671e2ddc57c
AlexOks <alex.oksyuta@gmail.com>
2019-08-01 13:53:00 +0300

    Basic setting up the server

---
+++ smart-app-api/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) =>{
+    res.send('working');
+})
+
+app.listen(3000, () => {
+    console.log('api is run on port 3000');
+})
+
+
+/*
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+*/
